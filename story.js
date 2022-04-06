644403d83f9fc4074eda09b3093eb27b6079cae8
Lilly <lillykassner@yahoo.de>
2022-04-06 14:09:53 +0200

    changes from map to pictures

--- story.js
+++ story.js
@@ -4,7 +4,9 @@ let text = document.getElementById("text");
 let buttonBox = document.getElementById('buttonBox');
 let input = document.getElementById('input');
 let inventory = document.getElementById('inventoryBox');
-
+let lifeCount = 4;
+let lifeEl = document.getElementById('lifeEl');
+let elements = document.getElementById('elements');
 let playerName;
 
 input.addEventListener("keypress", setName);
@@ -14,7 +16,8 @@ input.addEventListener("keypress", setName);
   console.log(input.value);
   if (event.key == "Enter" || event.keyCode == 13) {
     playerName =  input.value;
-    input.parentNode.removeChild(input)
+   // input.parentNode.removeChild(input)
+    input.style.display = "none";
     advanceTo(scenario.two)
   }
 };
@@ -33,6 +36,7 @@ const changeText = function(words) {
 const changeInventory = function(inv) {
   inventory.innerHTML += inv ;
 };
+
 
 
 let countSteps = 0;
@@ -56,158 +60,152 @@ let advanceTo = function(s) {
 let death = function(){
  
   let random = prompt("Please enter a number between 1 and 5");
-  let number = Math.trunc(Math.random()*5)
+  let number = Math.trunc(Math.random()*100)
+  console.log( random + "and" + number)
   if (random == number) {
   
     advanceTo(scenario.ten);
    
-  } else {
-    advanceTo(scenario.ten);
+  } else if(lifeCount >= 0){
+    console.log("hey")
+
+   elements.removeChild(lifeEl)
+   console.log(elements.childNodes)
+ 
+   // advanceTo(scenario.death);
     
   }
 }
 
-let finishGame = function(){
-  text.innerText = "YOU LIVE, HOW GREAT IS THAT?!"
-}
-
-function fight(){
-  let random = prompt("Please enter a number between 1 and 5");
-let number = Math.trunc(Math.random()*5)
-if (random == number) {
-  console.log("you win!")
- 
-} else {
-  console.log("you lose!")
-}
-
-}
-
-
 
 let scenario = {
   one: {
-   image: "/assets/zombie-map.png",
+   image: "/assets/start.png",
     text: "Hello, oh brave player! What is your name?\n",
     inventory: "",
   },
   two: {
-    image: "/assets/secondScenario-map.png",
+    image: "/assets/Cat-secondScenario.png",
     text: "Your name aka you awake by a sudden noise. Oh no! Zombies have entered your house and are coming for you. What do you do?",
     buttons: [["I hide in my bathroom", "advanceTo(scenario.three)"],["I run out of the door", "advanceTo(scenario.four)"]],
     inventory: "",
   },
   three: {
-    image: "/assets/thirdScenario-map.png",
+    image: "/assets/Cat-thirdScenario.png",
     text: "The zombies break through the door. You try to kill as many as you can with your toothbrush but its not enough.",
     buttons: [["climb through window  + run into the city", "advanceTo(scenario.five)"],["climb through window  + run into the forest", "advanceTo(scenario.six)"]],
     inventory: "",
   },
     four: {
-        image: "/assets/fourthScenario-map.png",
+        image: "/assets/Cat-fourthScenario.png",
     text: "You stand on the street. Do you go to the forest to hide in a hut or do you go to the street and head for the city maybe you can find some",
-    buttons: [["go to the street and head out to the town to find some information there", "advanceTo(scenario.five)"],["i head east to the forest to look for a hut to hide", "advanceTo(scenario.six)"]],
+    buttons: [["go to the street and head out to the town", "advanceTo(scenario.five)"],["i head to the forest to look for a hut to hide", "advanceTo(scenario.six)"]],
     inventory: "",
   },
     five: {
-        image: "/assets/fifthScenario-map.png",
+        image: "/assets/Cat-fifthScenario.png",
     text: "You are now in town and you hear a noise. You have to options to hide. Go into the supermarket or into the weapon shop",
     buttons: [["Supermarket", "advanceTo(scenario.seven)"],["Weapon-Shop", "advanceTo(scenario.eight)"]],
     inventory: "",
   },
 
   six: {
-    image: "/assets/sixthScenario-map.png",
+    image: "/assets/Cat-sixthScenario.png",
     text: "You enter the forest and start looking for the hut. After hours of searching you finally find it. Inside the hut is pure chaos but you find a map which leads to a secret and safe island. You are excited!",
-    buttons: [["I must go there immeditaley! I turn east and start walking", "advanceTo(scenario.ten)"],["First i rest. Tomorrow is a new day. I shall start my journey then!", "advanceTo(scenario.nine)"]],
+    buttons: [["I must go there immeditaley! ", "advanceTo(scenario.ten)"],["First i rest. Tomorrow I shall start my journey!", "advanceTo(scenario.nine)"]],
     inventory: "",
   },
 
   seven: {
-    image: "/assets/seventhScenario-map.png",
+    image: "/assets/Cat-seventhScenario.png",
     text: "You are now in the supermarket and find a whole back of food. Your lucky day! In the bag you also find a secret map leading to a safe place on an island! Lucky day indeed! You head straight east! ",
     buttons: [["Lets head east!", "advanceTo(scenario.ten)"]],
     inventory: "🥫🥫🥫",
   },
   eight: {
-    image: "/assets/eighthScenario-map.png",
+    image: "/assets/Cat-eighthScenario.png",
     text: "You are now in the weapon shop and find a bunch of knives. Your lucky day! Besides the kinves you also find a secret map leading to a safe place on an island! Lucky day indeed! You head straight east!",
     buttons: [["Lets head east!", "advanceTo(scenario.ten)"]],
     inventory: "🗡️🗡️🗡️",
   },
 
   nine: {
-    image: "/assets/ninethScenario-map.png",
+    image: "/assets/Cat-ninethScenario.png",
     text: "You lay down to sleep. In the night you hear disturbig noises. The hut was a mistake. You are surronded by zombies with no chance to esacpe. Oh poor you!",
     buttons: [["Now your fade is in your hands. Spin to find out", "death()"]],
     inventory: "",
   },
 
   ten: {
-    image: "/assets/tenthScenario-map.png",
+    image: "/assets/Cat-tenthScenario.png",
     text: "You start walking and come to a crossroad.",
     buttons: [["Left", "advanceTo(scenario.eleven)"],["Right", "advanceTo(scenario.twelve)"]],
     inventory: "",
   },
 
   eleven: {
-    image: "/assets/eleventhScenario-map.png",
+    image: "/assets/Cat-eleventhScenario.png",
     text: "You turn left and reach the port. Well done so far! You now the island must be straight ahead of you.",
     buttons: [["I charter a boat and head to the island", "advanceTo(scenario.thirteen)"],["I try to swim across.", "advanceTo(scenario.fourteen)"]],
     inventory: "",
   },
 
   twelve: {
-    image: "/assets/twelvethScenario-map.png",
+    image: "/assets/Cat-twelvethScenario.png",
     text: "You turn right and reach the city again. You are lost... Either you head back to the crossroad or try to find a faster way to a port",
     buttons: [["Go back", "advanceTo(scenario.fifteen)"],["Continue", "advanceTo(scenario.sixteen)"]],
     inventory: "",
   },
 
   thirteen: {
-    image: "/assets/thirteenthScenario-map.png",
+    image: "/assets/Cat-thirteenthScenario.png",
     text: "Congrats! You reach the island. There is a small community, which is happy to have a new member. You live a happy life!",
     buttons: [["Start Again", "location.reload();"]],
     inventory: "",
   },
 
   fourteen: {
-    image: "/assets/fourteenthScenario-map.png",
+    image: "/assets/Cat-fourteenthScenario.png",
     text: "Oh no! You were defintely to confident. You struggle enormesly",
     buttons: [["Call for help and try to pull through", "advanceTo(scenario.seventeen)"],["Try to swim back. Maybe its not to late", "advanceTo(scenario.eighteen)"]],
     inventory: "",
   },
 
   fifteen: {
-    image: "/assets/fifteenthScenario-map.png",
+    image: "/assets/Cat-fifteenthScenario.png",
     text: "You turn back  and reach the port eventuelly . Well done so far! You now the island must be straight ahead of you. You find a boat and start your successful jouney to the island. You are safe!",
     buttons: [["Start Again", "location.reload();"]],
     inventory: "",
   },
 
   sixteen: {
-    image: "/assets/sixteenthScenario-map.png",
-    text: "Oh no! You were defintely to confident. You meet a zombie group and become one of them. Game over",
-    buttons: [["Your fade as a zombie awaits you", "death()"]],
+    image: "/assets/Cat-sixteenthScenario.png",
+    text: "Oh no! You were defintely to confident. You meet a zombie group. What do you do? If you have food in your inventory you can distract the zombies with it and run away back to the crossroad. If you have a weapon you can enter the fight and suceed",
+    buttons: [["Let´s see if a fade as a zombie awaits you?", "death()"]],
+    input: "hhh",
     inventory: "",
   },
 
   seventeen: {
-    image: "/assets/seventeenthScenario-map.png",
+    image: "/assets/Cat-seventheenthScenario.png",
     text: "Congrats!someone on the island hears your call and rescues you. There is a small community, which is happy to have a new member. You live a happy life!",
     buttons: [["Start Again", "location.reload();"]],
     inventory: "",
   },
 
   eighteen: {
-    image: "/assets/eighteenthScenario-map.png",
-    text: "Well on the shore back you see the zombies already waiting for you. You become one of them",
+    image: "/assets/Cat-eighteenthScenario.png",
+    text: "Well on the shore back you see the zombies already waiting for you. Either you become one of them or you will be set back to the crossroad. ",
     buttons: [["Your fade as a zombie awaits you", "death()"]],
     inventory: "",
   },
 
-
-
+  death: {
+    image: "/assets/gameOver.png",
+    text: "GAME OVER",
+    buttons: [["Start AGain", "location.reload();"]],
+    inventory: "",
+  }
   
 };
 
